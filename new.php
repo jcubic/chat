0528c804d0bb410dd286d31a75de0e3bd5d18756
Jakub Jankiewicz <jcubic@onet.pl>
2023-05-21 10:45:40 +0200

    only mark bitcoin address as spam

--- new.php
+++ new.php
@@ -16,8 +16,5 @@ function is_spam($string) {
   if (preg_match("/\\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\\b/", $string)) {
     return true;
   }
-  if (preg_match("/\\b(BTC|bitcoin|crypto|donate|Support me)\\b/i", $string)) {
-    return true;
-  }
   return false;
 }
