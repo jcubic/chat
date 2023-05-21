130284b89b99869dd1075699747f9183fddfbe94
Jakub Jankiewicz <jcubic@onet.pl>
2023-05-21 10:40:57 +0200

    spam protection

--- new.php
+++ new.php
@@ -3,7 +3,21 @@
 if (isset($_POST['message']) && isset($_POST['username'])) {
   require_once('Messages.php');
 
+  if (is_spam($_POST['message']) || is_spam($_POST['username'])) {
+    die;
+  }
   $messages = new Messages();
   $messages->newMessage($_POST['username'], $_POST['message']);
 }
 
+
+
+function is_spam($string) {
+  if (preg_match("/\\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\\b/", $string)) {
+    return true;
+  }
+  if (preg_match("/\\b(BTC|bitcoin|crypto|donate|Support me)\\b/i", $string)) {
+    return true;
+  }
+  return false;
+}
